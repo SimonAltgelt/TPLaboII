694e613d1fce0ca58b51dbb34e1adf1650aabf78
SimonAltgelt <146289023+SimonAltgelt@users.noreply.github.com>
2023-10-11 10:43:46 -0300

    Primera subida

--- main.cpp
+++ main.cpp
@@ -44,7 +44,6 @@ int main(){
             break;
             case 0: return 0;
             break;
-
             default: cout<<"OPCION INCORRECTA. INGRESAR OPCION NUEVAMENTE ";
             break;
         }
